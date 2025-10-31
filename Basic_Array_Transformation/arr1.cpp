41a177621681646defd9d9664de27d4d812737c2
SakshiSupekar07 <supekarsakshj07@gmail.com>
2025-10-31 10:41:04 +0530

    Implement map function for array transformation
    Add a map function to transform array elements using a callback.

--- Basic_Array_Transformation/arr1.cpp
+++ Basic_Array_Transformation/arr1.cpp
@@ -1,1 +1,13 @@
-
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let newArr=[]
+    arr.forEach((value,index)=>
+    {
+        newArr.push(fn(value,index));
+    })
+    return newArr;
+};
